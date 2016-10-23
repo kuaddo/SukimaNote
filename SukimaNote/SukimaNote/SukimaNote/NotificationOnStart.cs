e2819ac67cb7bfd72426f33ddc9d9eabd842678b
yudaikichise <yudaikichise@gmail.com>
2016-10-23 23:03:09 +0900

    謎のエラー出るのでコメントして保存します。。

--- SukimaNote/SukimaNote/SukimaNote/NotificationOnStart.cs
+++ SukimaNote/SukimaNote/SukimaNote/NotificationOnStart.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using PCLStorage;
 
+/*
 
 namespace SukimaNote
 {
 	public class NotificationOnStart
 	{
 		private int[,] table = new int[7,24];
-		public  NotificationOnStart()
+		NotificationOnStart()
 		{
-			
+			async () =>
+			{
+				// ルートフォルダを取得
+				IFolder rootfolder = FileSystem.Current.LocalStorage;
+				// ルート直下にフォルダを作成（すでにある場合はそれをオープン）
+				IFolder TableFolder = await IFolder.createFolderAsync("Table", CreationCollisionOption.OpenIfExists);
+				// ファイルが存在するかを確かめる
+				ExistenceCheckResult ret = TableFolder.CheckExistsAsync("Table.txt");
+				if (ret.FileExists == ExistenceCheckResult.FileExists)
+				{
+					
+				}
+
+			}
 		}
+
+
 	}
 
 }
+
+*/
