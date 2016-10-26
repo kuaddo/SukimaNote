147124a751779b4733750a00a6e9e3ac2cefc324
yudaikichise <yudaikichise@gmail.com>
2016-10-26 23:49:56 +0900

    ファイルの呼び出し手前まで

--- SukimaNote/SukimaNote/SukimaNote/NotificationOnStart.cs
+++ SukimaNote/SukimaNote/SukimaNote/NotificationOnStart.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using PCLStorage;
 
-/*
+
 
 namespace SukimaNote
 {
@@ -10,25 +10,48 @@ namespace SukimaNote
 		private int[,] table = new int[7,24];
 		NotificationOnStart()
 		{
-			async () =>
-			{
-				// ルートフォルダを取得
-				IFolder rootfolder = FileSystem.Current.LocalStorage;
-				// ルート直下にフォルダを作成（すでにある場合はそれをオープン）
-				IFolder TableFolder = await IFolder.createFolderAsync("Table", CreationCollisionOption.OpenIfExists);
-				// ファイルが存在するかを確かめる
-				ExistenceCheckResult ret = TableFolder.CheckExistsAsync("Table.txt");
-				if (ret.FileExists == ExistenceCheckResult.FileExists)
-				{
-					
-				}
-
-			}
+			FuncAsync();
 		}
 
+		public async void FuncAsync()
+		{
+			// appのルートフォルダを取得
+			IFolder rootFolder = FileSystem.Current.LocalStorage;
 
+			// ルート直下にサブフォルダを作成
+			IFolder tableFolder = await rootFolder.CreateFolderAsync("table", CreationCollisionOption.OpenIfExists);
+
+			// table.txtが存在するかを確認する
+			ExistenceCheckResult res = await tableFolder.CheckExistsAsync("table.txt");
+
+			if (res == ExistenceCheckResult.FileExists)
+			{
+				// table.txtが存在する場合
+				// table.txtを読み込んで、配列に格納する
+
+
+			}
+			else {
+				// table.txtが存在しない場合
+				// 配列を初期化する
+				for (int i = 0; i < 7; i++)
+				{
+					for (int j = 0; j < 24; j++)
+					{
+						table[i,j] = 0;
+					}
+				}
+			}
+			// 配列の用意が完了する
+			// 現在時刻を取得し、曜日と時間を用意する
+
+
+			// 配列の値を変更する
+
+
+
+		}
 	}
 
 }
 
-*/
