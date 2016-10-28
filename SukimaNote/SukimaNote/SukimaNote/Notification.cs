99e39d0dfaf4e7326e7b06355136f9d7abc31c47
yudaikichise <yudaikichise@gmail.com>
2016-10-28 23:46:59 +0900

    PCLStorageの扱いは保留、固有APIの使用の手前まで記述。
    Conflicts:
    	SukimaNote/SukimaNote/SukimaNote.Droid/Resources/Resource.Designer.cs

---
+++ SukimaNote/SukimaNote/SukimaNote/Notification.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using PCLStorage;
+
+
+
+namespace SukimaNote
+{
+	public class Notification
+	{
+		
+
+
+		public async void OnStartAsync()
+		{
+
+			int[,] table = new int[7, 24];
+			// appのルートフォルダを取得
+			IFolder rootFolder = FileSystem.Current.LocalStorage;
+
+			// ルート直下にサブフォルダを作成
+			IFolder tableFolder = await rootFolder.CreateFolderAsync("table", CreationCollisionOption.OpenIfExists);
+
+			// table.txtが存在するかを確認する
+			ExistenceCheckResult res = await tableFolder.CheckExistsAsync("table.txt");
+
+			if (res == ExistenceCheckResult.FileExists)
+			{
+				// table.txtが存在する場合
+				// table.txtを読み込んで、配列に格納する
+				// まず文字列変数に文字を格納してから、値の取り出しを行おうかなー
+
+			}
+			else {
+				// table.txtが存在しない場合
+				// 配列を初期化する
+				for (int i = 0; i < 7; i++)
+				{
+					for (int j = 0; j < 24; j++)
+					{
+						table[i,j] = 0;
+					}
+				}
+			}
+			// 配列の用意が完了する
+			// 現在時刻を取得し、曜日と時間を用意する
+			DateTime LogInTime = DateTime.Now;
+			int LogInHour = LogInTime.Hour;
+			if (LogInHour > 23) LogInHour = 23;
+			if (LogInHour < 0) LogInHour = 0;
+			int LogInDayOfWeek = 0; // セグメンテーション違反を防ぐため初期値0
+			switch (LogInTime.DayOfWeek)
+			{
+				case DayOfWeek.Sunday: LogInDayOfWeek = 0; break;
+				case DayOfWeek.Monday: LogInDayOfWeek = 1; break;
+				case DayOfWeek.Tuesday: LogInDayOfWeek = 2; break;
+				case DayOfWeek.Wednesday: LogInDayOfWeek = 3; break;
+				case DayOfWeek.Thursday: LogInDayOfWeek = 4; break;
+				case DayOfWeek.Friday: LogInDayOfWeek = 5; break;
+				case DayOfWeek.Saturday: LogInDayOfWeek = 6; break;
+				default: break;
+			}
+
+			// 時刻と曜日の取得が完了
+
+			// 配列の値を変更する
+
+			table[LogInDayOfWeek, LogInHour] += 3; // 起動した時間と曜日が指す位置の値を増加
+			// 他の曜日の同じ時間の値も増加
+			for (int i = 0; i < 7; i++)
+			{
+				table[i, LogInHour] += 1; 
+			}
+
+			// オーバーフローを防ぐために要素の値がある一定値以上になったら全部の要素の値を減らしましょうっていう処理
+			if (table[LogInDayOfWeek, LogInHour] > 10000)
+			{
+				for (int i = 0; i < 7; i++)
+				{
+					for (int j = 0; j < 24; j++)
+					{
+						if (table[LogInDayOfWeek, LogInHour] > 1000)
+						{
+							table[LogInDayOfWeek, LogInHour] -= 1000;
+
+						}
+						else {
+							table[LogInDayOfWeek, LogInHour] = 0;
+						}
+					}
+				}
+			}
+
+			// オーバーフロー防止処理終了
+
+			// 通知時刻決定処理
+			// 何時に通知したいかを決めて、その差分を計算するメソッドに通知したい時間を投げる <-- まずはこのメソッドで実装します...
+			int NotificationDOW = LogInDayOfWeek + 1;
+			if (NotificationDOW == 7)
+			{
+				NotificationDOW = 0;
+			}
+			int NotificationHour = 0;
+			for (int i = 0; i < 24; i++)
+			{
+				if (table[NotificationDOW, i] > table[NotificationDOW, NotificationHour])
+				{
+					NotificationHour = i;
+				}
+			}
+
+			// 翌日の通知を投げる時刻を決定した
+			// 通知時間までの差分を求めよう(とりあえずint型)
+
+			int interval = 3600 * (24 - LogInHour + NotificationHour); // これで通知時間までのざっくりした時間が求まる
+
+						
+		}
+	}
+
+}
+
